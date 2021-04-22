173951c1a3ce7b8c222fdc66269abd8a98b475f1
RoshanLiu <xygenio@hotmail.com>
2021-04-22 22:06:10 +1000

    imac

---
+++ app/src/main/java/com/roshan/feelthepain/ui/slideshow/SlideshowViewModel.java
@@ -0,0 +1,19 @@
+package com.roshan.feelthepain.ui.slideshow;
+
+import androidx.lifecycle.LiveData;
+import androidx.lifecycle.MutableLiveData;
+import androidx.lifecycle.ViewModel;
+
+public class SlideshowViewModel extends ViewModel {
+
+    private MutableLiveData<String> mText;
+
+    public SlideshowViewModel() {
+        mText = new MutableLiveData<>();
+        mText.setValue("This is slideshow fragment");
+    }
+
+    public LiveData<String> getText() {
+        return mText;
+    }
+}
